e0e09a29034e4d6aa17c979c9921528e6dcf0f0e
nguyenlongCS <thanhlong86.tt@gmail.com>
2025-09-14 08:54:11 +0700

    temp

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
 /*
 src/firebase/config.js - Refactored
 Cấu hình Firebase với database: social-media-web-database và Storage bucket
+Sửa RTDB để sử dụng database social-media-web-rtdb
 */
 
 import { initializeApp } from 'firebase/app'
@@ -18,7 +19,7 @@ const firebaseConfig = {
   messagingSenderId: "306302302026",
   appId: "1:306302302026:web:63ffe859d4cbbeda6073b3",
   measurementId: "G-SSWWQ42BLN",
-  databaseURL: "https://fir-auth-cozy-default-rtdb.asia-southeast1.firebasedatabase.app"
+  databaseURL: "https://social-media-web-rtdb-default-rtdb.asia-southeast1.firebasedatabase.app"
 }
 
 // Khởi tạo Firebase App
@@ -33,6 +34,7 @@ export const db = getFirestore(app, 'social-media-web-database')
 // Khởi tạo Storage với bucket tùy chỉnh cho social-media-web
 export const storage = getStorage(app, 'gs://social-media-web-storage')
 
-export const rtdb = getDatabase(app, 'social-media-web-rtdb')
+// Khởi tạo Realtime Database với database cụ thể: social-media-web-rtdb
+export const rtdb = getDatabase(app, 'https://fir-auth-cozy-default-rtdb.asia-southeast1.firebasedatabase.app')
 
 export default app
